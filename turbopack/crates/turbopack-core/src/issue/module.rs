b03468612fc3724182ff7cba29212c898869b6b7
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-05 17:21:38 +0100

    Turbopack: Support data URI sources

--- turbopack/crates/turbopack-core/src/issue/module.rs
+++ turbopack/crates/turbopack-core/src/issue/module.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use turbo_tasks::{ResolvedVc, Vc};
+use turbo_tasks::{ResolvedVc, ValueToString, Vc};
 use turbo_tasks_fs::FileSystemPath;
 
 use super::{Issue, IssueStage, OptionStyledString, StyledString};
@@ -41,9 +41,9 @@ pub async fn emit_unknown_module_type_error(source: Vc<Box<dyn Source>>) -> Resu
         ident: source.ident().to_resolved().await?,
         title: StyledString::Text("Unknown module type".into()).resolved_cell(),
         description: StyledString::Text(
-            r"This module doesn't have an associated type. Use a known file extension, or register a loader for it.
+            format!("The module \'{}\' doesn't have an associated type. Use a known file extension, or register a loader for it.
 
-Read more: https://nextjs.org/docs/app/api-reference/next-config-js/turbo#webpack-loaders".into(),
+Read more: https://nextjs.org/docs/app/api-reference/next-config-js/turbo#webpack-loaders",  source.ident().to_string().await?).into(),
         )
         .resolved_cell(),
     }
